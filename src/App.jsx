e65a03d08d88e8d9d3604a98c089d459e9b8ca0a
Rasya Devansyah <rasya.devan21@gmail.com>
2025-03-12 20:03:54 +0700

    Hash Router Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import SignInPage from './pages/SignInPage'
 import SignUpPage from './pages/SignUpPage';
 import HomePage from './pages/HomePage';
-import { Route, Routes } from 'react-router-dom';
-
+// import { Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <main>
+    <Router>
+      <main>
         <Routes>
-          <Route path="/" element={<SignInPage />}/>
-          <Route path="/SignUp" element={<SignUpPage/>}/>
-          <Route path="/Home" element={<HomePage/>} />
-          <Route/>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/SignUp" element={<SignUpPage />} />
+          <Route path="/Home" element={<HomePage />} />
+          <Route />
         </Routes>
-    </main>
+      </main>
+    </Router>
+
   )
 }
 
