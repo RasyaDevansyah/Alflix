a3229257016737438c6c526a77c9550ceddf7b9f
Rasya Devansyah <rasya.devan21@gmail.com>
2025-03-12 19:24:22 +0700

    Home Page Progress

--- src/components/AlflixLogo.jsx
+++ src/components/AlflixLogo.jsx
@@ -1,12 +1,12 @@
 // if type 2 => "..\src\assets\Alflix Logo 2.png"
 // if type 1 => "..\src\assets\Alflix Logo.png"
 
-const AlflixLogo = ({ type }) => {
+const AlflixLogo = ({ type, className }) => {
     // Determine the image source based on the type prop
     const imageSrc = type === "2" ? "../src/assets/Alflix Logo 2.png" : "../src/assets/Alflix Logo.png";
 
     return (
-        <img src={imageSrc} alt="Alflix Logo" />
+        <img src={imageSrc} alt="Alflix Logo" className={className} />
     );
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,69 @@
+import AlflixLogo from "../components/AlflixLogo"
+
+function HomePage() {
 
 
-function HomePage(){
+    return (
+        <div className="min-h-screen bg-[#1e1e2a] text-white">
+
+            {/* Navbar */}
+            <div className="flex h-14 justify-between">
+                {/* Navbar Left */}
+                <div className="flex items-center">
+                    <AlflixLogo type='2' className="w-1/5 h-auto mr-4 ml-3"/>
+                    <div className="h-9 w-px bg-white mx-4"></div>
+                    <p className="mx-4 font-bold ">HOME</p>
+                    <p className="mx-4 font-bold">BROWSE</p>
+                    <p className="mx-4 font-bold">SUBSCRIPTION</p>
+
+                </div>
+
+                {/* Navbar Right */}
+                <div className="flex items-center">
+                    <p className="mx-4">right1</p>
+                    <p className="mx-4">right2</p>
+                    <p className="mx-4">right3</p>
+                </div>
 
 
-    return <></>
+            </div>
+
+
+
+
+            <div className="banner">
+                <p>banner</p>
+            </div>
+
+            <div className="trendingsection">
+                <p>Trending Section</p>
+
+            </div>
+
+            <div className="movieRowsSection">
+
+                <div className="movieRow">
+
+                </div>
+
+                <div className="movieRow">
+
+                </div>
+
+                <div className="movieRow">
+
+                </div>
+
+
+
+            </div>
+
+
+
+
+
+        </div>
+    )
 
 
 }
