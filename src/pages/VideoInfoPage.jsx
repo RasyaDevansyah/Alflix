376bf5d9586056d23802f8659166782fa4823b44
Rasya Devansyah <rasya.devan21@gmail.com>
2025-03-19 09:03:23 +0700

    VideoInfoPage Progress

---
+++ src/pages/VideoInfoPage.jsx
@@ -0,0 +1,55 @@
+import Navbar from "../components/HomePage/Navbar"
+
+import Footer from "../components/Footer"
+
+
+function VideoInfoPage(){
+
+
+    return (
+        <div className="min-h-screen bg-[#1e1e2a] text-white font-libre-franklin">
+            <Navbar/>
+
+            {/* Banner */}
+            <div>
+
+            </div>
+
+            {/* Title Section */}
+            <div>
+
+
+            </div>
+
+            {/* Description Section */}
+            <div>
+
+            </div>
+
+
+            {/* Cast Section */}
+            <div>
+
+            </div>
+
+
+            {/* More Like this Section */}
+            <div>
+                
+            </div>
+
+            <Footer/>
+
+
+
+
+
+        </div>
+
+
+    )
+
+}
+
+
+export default VideoInfoPage
