09d508a4f5071a0aed9cb944e6cd403bb91b9287
GregoriusWilliam <gregoriuswilliam1@gmail.com>
2025-03-20 11:30:44 +0700

    SubcriptionPage
    ini buat link ke subs page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import SignInPage from './pages/SignInPage'
 import SignUpPage from './pages/SignUpPage';
 import HomePage from './pages/HomePage';
+import SubscriptionPage from "./pages/SubscriptionPage"; 
 // import { Route, Routes } from 'react-router-dom';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from "./components/HomePage/Navbar";
 
 function App() {
 
@@ -12,6 +15,7 @@ function App() {
           <Route path="/" element={<SignInPage />} />
           <Route path="/SignUp" element={<SignUpPage />} />
           <Route path="/Home" element={<HomePage />} />
+          <Route path="/Subscription" element={<SubscriptionPage />} />
         </Routes>
       </main>
 
