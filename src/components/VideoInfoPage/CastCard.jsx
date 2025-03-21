d02dbc3c370e5c8f0cd5f5a404ce6615e54081e0
Rasya Devansyah <rasya.devan21@gmail.com>
2025-03-21 18:40:25 +0700

    VideoInfoPage Progress scroll bar

--- src/components/VideoInfoPage/CastCard.jsx
+++ src/components/VideoInfoPage/CastCard.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function CastCard({ imageSrc, castName, characterName }) {
     return (
-        <div className="flex flex-col gap-y-1 w-45 h-70">
+        <div className="flex flex-col w-70 h-80 relative flex-shrink-0">
             {/* Picture card */}
-            <div className="max-w-45 max-h-55 rounded-lg overflow-hidden shadow-[0_0_10px_1px_rgba(255,255,255,0.2)]">
+            <div className="max-w-70 max-h-75 rounded-lg overflow-hidden shadow-[0_0_10px_1px_rgba(255,255,255,0.2)]">
                 <img 
                     src={imageSrc} 
                     alt="ActorImage" 
@@ -13,10 +13,10 @@ function CastCard({ imageSrc, castName, characterName }) {
             </div>
 
             {/* Cast Name */}
-            <p className="text-white font-semibold">{castName}</p>
+            <p className="mt-1 text-xl leading-[1.6] tracking-[0.15em] font-bold">{castName}</p>
 
             {/* Character Name */}
-            <p className="text-sm text-[#B8B1FF]">{characterName}</p>
+            <p className="text-sm text-[#B8B1FF] tracking-[0.15em]">{characterName}</p>
         </div>
     );
 }
