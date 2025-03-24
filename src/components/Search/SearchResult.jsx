95118087bcf303041685278bacee1a94b095ddb2
ChristianS <christianzxz123@gmail.com>
2025-03-24 21:23:13 +0700

    SearchBar

---
+++ src/components/Search/SearchResult.jsx
@@ -0,0 +1,11 @@
+    import React from 'react';
+
+    const SearchResult = () => {
+        return (
+            <div className='fixed top-18 left-[80%] z-20 w-1/6 rounded-2xl h-14 p-3 shadow-lg flex items-center bg-gray-800/30 backdrop-blur-sm group focus-within:bg-gray-800 dark:focus-within:bg-gray-900 transition-colors duration-1000'>
+                
+            </div>
+        );
+    };
+
+    export default SearchResult;
