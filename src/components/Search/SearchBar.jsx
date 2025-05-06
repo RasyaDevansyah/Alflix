7681779adc9426a820ab1a4ab2397ca528afc099
ChristianS <christianzxz123@gmail.com>
2025-05-06 14:03:28 +0700

    Search bar and search result

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -1,12 +1,32 @@
     import React, { useState } from 'react';
     import { FaSearch } from 'react-icons/fa';
 
-    const SearchBar = ({searchResult}) => {
+    const SearchBar = ({searchResult, setIsSearchActive}) => {
         const [input, setInput] = useState("")
 
         const handleChange = (value) => {
             setInput(value)
-            console.log(value)
+            const movies = [ // dummy movie
+                "Avengers: Endgame",
+                "Beauty and the Beast",
+                "The Lion King",
+                "Frozen",
+                "Toy Story",
+                "Finding Nemo",
+                "Moana",
+                "Joker",
+                "The Mandolarian",
+                "Spirited Away",
+                "Thor: Ragnarok",
+                "Black Panther"
+            ];
+
+            const filteredMovies = movies.filter(movie =>
+                movie.toLowerCase().includes(value.toLowerCase())
+            );
+
+            console.log(filteredMovies);
+            searchResult(filteredMovies);
         };
 
 
@@ -17,7 +37,11 @@
                 type="text" 
                 placeholder='Type to search ...' 
                 className='bg-transparent border-none outline-none text-base ml-2 w-full text-gray-400 placeholder:text-gray-400 focus:placeholder:text-white focus:text-white transition-colors duration-1000'
-                onChange={(e) => handleChange(e.target.value)}/>
+                onChange={(e) => handleChange(e.target.value)}
+                onFocus={() => setIsSearchActive(true)}
+                onBlur={() => setTimeout(() => setIsSearchActive(false), 200)}
+                />
+    
             </div>
         );
     };
