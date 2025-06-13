f4761cfd2394b15a0b8f7964d50639ba8338e8bb
GregoriusWilliam <gregoriuswilliam1@gmail.com>
2025-06-13 15:11:18 +0700

    VideoPlayback

--- src/pages/VideoInfoPage.jsx
+++ src/pages/VideoInfoPage.jsx
@@ -1,12 +1,16 @@
-import Navbar from "../components/HomePage/Navbar"
-import PlayBanner from "../components/VideoInfoPage/PlayBanner"
-import VideoTitleInfo from "../components/VideoInfoPage/VideoTitleInfo"
-import DescriptionSection from "../components/VideoInfoPage/DescriptionSection"
-import CastSection from "../components/VideoInfoPage/CastSection"
-import RelatedMovies from "../components/VideoInfoPage/RelatedMovies"
-import Footer from "../components/Footer"
+import Navbar from "../components/HomePage/Navbar";
+import PlayBanner from "../components/VideoInfoPage/PlayBanner";
+import VideoTitleInfo from "../components/VideoInfoPage/VideoTitleInfo";
+import DescriptionSection from "../components/VideoInfoPage/DescriptionSection";
+import CastSection from "../components/VideoInfoPage/CastSection";
+import RelatedMovies from "../components/VideoInfoPage/RelatedMovies";
+import Footer from "../components/Footer";
+
 
 import beautyAndTheBeast from '/src/assets/BeautyAndTheBeast.jpeg';
+
+import beautyAndTheBeast2 from '/src/assets/BeautyAndTheBeast2.png';
+
 
 import EmmaWatson from '/src/assets/MovieCasts/Emma Watson.png';
 import DanStevens from '/src/assets/MovieCasts/Dan Stevens.png';
@@ -14,9 +18,10 @@ import LukeEvans from '/src/assets/MovieCasts/Luke Evans.png';
 import EwanMcGregor from '/src/assets/MovieCasts/Ewan McGregor.png';
 import EmmaThomson from '/src/assets/MovieCasts/Emma Thompson.png';
 
-import Aladin from '/src/assets/MoviePostersLandscape/Aladin.jpeg'
-import LionKing from '/src/assets/MoviePostersLandscape/Lion King.png'
-import SnowWhite from '/src/assets/MoviePostersLandscape/SnowWhite.png'
+
+import Aladin from '/src/assets/MoviePostersLandscape/Aladin.jpeg';
+import LionKing from '/src/assets/MoviePostersLandscape/Lion King.png';
+import SnowWhite from '/src/assets/MoviePostersLandscape/SnowWhite.png';
 
 function VideoInfoPage() {
   const movieData = {
@@ -24,9 +29,10 @@ function VideoInfoPage() {
     year: "2020",
     rating: "7.9",
     genres: ["Romance", "Fantasy", "Disney"],
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     quote: `“Come and\nsee the\nmagic\nbestowed.”`,
-    image: beautyAndTheBeast
+    image: beautyAndTheBeast,
   };
 
   const castMembers = [
@@ -43,18 +49,15 @@ function VideoInfoPage() {
     { image: LionKing, title: "LION KING" },
   ];
 
-
   return (
     <div className="min-h-screen bg-[#1e1e2a] text-white font-libre-franklin">
       <Navbar />
-      <PlayBanner />
-      <VideoTitleInfo {...movieData}/>
-
+      <PlayBanner poster={beautyAndTheBeast2} />
+      <VideoTitleInfo {...movieData} />
       <DescriptionSection
         description={movieData.description}
         quote={movieData.quote}
       />
-      
       <CastSection cast={castMembers} />
       <RelatedMovies movies={relatedMovies} />
       <Footer />
@@ -62,4 +65,4 @@ function VideoInfoPage() {
   );
 }
 
-export default VideoInfoPage
+export default VideoInfoPage;
