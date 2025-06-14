99194273fa6d05d3ec0985ebf74d65732df78c36
Rasya Devansyah <rasya.devan21@gmail.com>
2025-06-14 10:13:22 +0700

    Backend user and subscription progress

---
+++ backend/models/userdetail.model.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+const userDetailSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    watched: {
+        type: Number,
+        default: 0,
+    },
+    subId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Subscription",
+    },
+    watchHours: [
+        {
+            date: {
+                type: Date,
+                required: true,
+            },
+            duration: {
+                type: Number,
+                required: true,
+            },
+        }
+    ],
+    history: [
+        {
+            movieId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Movie",
+                required: true,
+            },
+            movieTags: [
+                {
+                    tagId: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "Tag",
+                        required: true,
+                    },
+                    tagName: {
+                        type: String,
+                        required: true,
+                    }
+                }
+            ],
+            timestamp: {
+                type: Date,
+                default: Date.now,
+            }
+        }
+    ]
+}, {
+    timestamps: true
+});
+
+const UserDetail = mongoose.model("UserDetail", userDetailSchema);
+
+export default UserDetail;
