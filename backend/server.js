ec7b38de42771f7e3d20f8dacf15d152825eb8a8
RasyaDevansyah <rasya.devan21@gmail.com>
2025-06-17 21:16:09 +0700

    uhhh final?

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(session({
     cookie: {
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        sameSite: 'lax',
     }
 }));
 
